27069d33172446ebaa9af8e9b5c597a47c48162c
Aniket <aniketsahran3@gmail.com>
2022-11-02 17:38:20 +0530

    Folder Structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './components/App';
 
-const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+);
