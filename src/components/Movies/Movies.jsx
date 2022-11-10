050ca0d557860273a628c472709b19373bd30531
Aniket <aniketsahran3@gmail.com>
2022-11-10 15:55:07 +0530

    Redux Setup Done

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Box, CircularProgress, useMediaQuery, Typography } from '@material-ui/core';
+import { useSelector } from 'react-redux';
+
+import { useGetMoviesQuery } from '../../services/TMDB';
 
 const Movies = () => {
+  const { data } = useGetMoviesQuery();
+  console.log(data);
+  
   return (
     <div>Movies</div>
   )
--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Provider } from 'react-redux';
 
 import App from './components/App';
+import store from './app/store';
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
+const theme = createTheme({});
 
 root.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <Provider store={store}>
+        <ThemeProvider theme={theme}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </ThemeProvider>
+    </Provider>
 );
