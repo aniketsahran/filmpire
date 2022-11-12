958e02e932dafabfb9bdb64d3aa83f3f85da8c1f
Aniket <aniketsahran3@gmail.com>
2022-11-13 02:23:59 +0530

    Actors Page Completed

---
+++ src/components/Actors/styles.js
@@ -0,0 +1,10 @@
+import { makeStyles } from '@material-ui/styles';
+
+export default makeStyles ((theme) => ({
+    image: {
+        maxWidth: '90%',
+        borderRadius: '20px',
+        objectFit: 'cover',
+        boxShadow: '0.5em 0.5em 1em',
+    },
+}));
